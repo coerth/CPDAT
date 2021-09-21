cefa30e641072aa380486ee0fb2c01368b92821a
Bradlestix <90347657+Bradlestix@users.noreply.github.com>
2021-09-21 15:39:52 +0200

    Opgaver

---
+++ Semester 1/Torsdagsopgaver/uge38-2021/Task1/Main.java
@@ -0,0 +1,23 @@
+public class Main{
+
+public static void main(String[] args) {
+  System.out.println();
+
+  SkrivNoget1("Dette er en string");
+  SkrivNoget2("Casper", 26);
+
+}
+
+public static void SkrivNoget1(String string){
+
+System.out.println(string);
+
+}
+
+public static void SkrivNoget2(String navn, int age){
+
+System.out.println("My name is " + navn + ", i am " + age + " years old");
+
+}
+
+}
